647a3dd746d890c2651ed419802f45cda61f0b6c
Dussavarun <varundussa@gmail.com>
2025-05-31 23:46:31 +0530

    commit to deploy

--- backend/sockets/socket.js
+++ backend/sockets/socket.js
@@ -1,11 +1,12 @@
-import {io} from "socket.io-client";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 
-//This creates a socket connection the backend server running at http://localhost:3000 
-const socket = io("http://localhost:3000", {
-    autoConnect: false,
-    reconnection: true,
-    reconnectionAttempts: 5,
-    reconnectionDelay: 1000
+const socket = io(BACKEND_URL, {
+  autoConnect: false,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+  transports: ['websocket'],
+  withCredentials: true
 });
 
 
